70b08b392bd5b34e74a6a876f7165e634a0d6302
Lorene_Valencia <ladymirene@proton.me>
2026-01-03 23:04:24 +0800

    Resolutions Class

--- Models/Resolutions.cs
+++ Models/Resolutions.cs
@@ -2,5 +2,10 @@
 {
     public class Resolutions
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsDone { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
     }
 }
